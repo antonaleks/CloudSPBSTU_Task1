4a9b3e0fdb79a33a9f37d9b62fd84a9a894384c4
armitageee <illidans7r@gmail.com>
2023-03-13 12:40:50 +0300

    my branch

---
+++ ansible-practice-realtime/server_cfg/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import Flask
+from crypt import methods
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello_world():
+    return "<p>Hello, bobrik!</p>"
+
+@app.route("/change_login", methods=['PUT'])
+def change_login():
+        return f"<p>new login is {request.form['login']}!</p>\n"
+
+@app.route("/login", methods=['POST'])
+def login():
+        return f"<p>Your login is {request.form['login']}</p>\n"
+
+
+app.run(host='0.0.0.0', port=5000)
