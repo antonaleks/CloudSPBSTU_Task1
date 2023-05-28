8c25cf35e8ce7e017761e79f48ad7e76488c88fe
celjon <134550766+celjon@users.noreply.github.com>
2023-05-28 13:28:40 +0300

    Update app.py

--- solution/app.py
+++ solution/app.py
@@ -3,16 +3,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def hello_world():
-    return "<p>Hello, World!</p>"
-
 @app.route("/", methods=['PUT'])
 def hello_world_put():
-    return "<p>Hello, World!</p>"
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.get_json()
+        return json
+    else:
+        return 'Content-Type not supported!'
 
 @app.route("/", methods=['POST'])
 def hello_world_post():
-    return "<p>Hello, World!</p>"
-
-app.run(host='0.0.0.0', port=5000)
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.get_json()
+        return json
+    else:
+        return 'Content-Type not supported!'
+    
+    app.run(host='0.0.0.0', port=5000)
