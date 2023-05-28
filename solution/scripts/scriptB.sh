ba22a7fdd823bb971324cd810a093028ed759839
celjon <134550766+celjon@users.noreply.github.com>
2023-05-28 14:07:36 +0300

    Update scriptB.sh

--- solution/scripts/scriptB.sh
+++ solution/scripts/scriptB.sh
@@ -1,9 +1,19 @@
-ip link add macvlan1 link eth0 type macvlan mode bridge # создаем адаптер bridge
-ip address add dev macvlan1 192.168.28.1/24 # ip адаптера
-ip link set macvlan1 up # включаем адаптер
+IP1=192.168.28.1/24
+IP2=192.168.7.1/24
 
-ip link add macvlan2 link eth0 type macvlan mode bridge # создаем адаптер bridge
-ip address add dev macvlan2 192.168.7.1/24 # ip адаптера
-ip link set macvlan2 up # включаем адаптер
+echo '# создаем адаптер bridge'
+ip link add macvlan1 link eth0 type macvlan mode bridge 
+echo '# добавляем ip адаптеру'
+ip address add dev macvlan1 $IP1 
+echo '# включаем адаптер'
+ip link set macvlan1 up 
 
-ip route show # проверяем порты
+echo '# создаем адаптер bridge'
+ip link add macvlan2 link eth0 type macvlan mode bridge 
+echo '# добавляем ip адаптеру'
+ip address add dev macvlan2 $IP2 
+echo '# включаем адаптер'
+ip link set macvlan2 up 
+
+echo '# проверяем порты'
+ip route show 
