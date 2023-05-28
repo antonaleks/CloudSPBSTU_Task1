dffd4a96ea98138b489e1c4042672845ce4896f6
celjon <134550766+celjon@users.noreply.github.com>
2023-05-28 13:42:55 +0300

    Update scriptA.sh

--- solution/scripts/scriptA.sh
+++ solution/scripts/scriptA.sh
@@ -1,8 +1,12 @@
-ip link add macvlan1 link eth0 type macvlan mode bridge # создаем адаптер тип bridge
-ip address add dev macvlan1 192.168.28.10/24 # добавляем ему ip
-ip link set macvlan1 up # включаем 
+echo 'создаем адаптер тип bridge'
+ip link add macvlan1 link eth0 type macvlan mode bridge 
+echo '# добавляем ему ip'
+ip address add dev macvlan1 192.168.28.10/24 
+echo '# включаем адаптер'
+ip link set macvlan1 up 
 
-ip route add 192.168.7.0/24 via 192.168.28.1 # маршрут к вм С через вм В
+echo '# маршрут к вм С через вм В'
+ip route add 192.168.7.0/24 via 192.168.28.1 
 
 pip install flask
 
