773e2528149761287089e38871dcf57ca08a0105
celjon <134550766+celjon@users.noreply.github.com>
2023-05-28 14:12:35 +0300

    Update scriptC.sh

--- solution/scripts/scriptC.sh
+++ solution/scripts/scriptC.sh
@@ -1,7 +1,15 @@
-ip link add macvlan1 link eth0 type macvlan mode bridge # создаем адаптер с типом bridge
-ip address add dev macvlan1 192.168.7.10/24 # ip адрес адаптера
-ip link set macvlan1 up # включаем адаптер
+IPADDR=192.168.7.10/24
+GETEWAY=192.168.7.1
+SUBNET=192.168.28.0/24
 
-ip route add 192.168.28.0/24 via 192.168.7.1 # добавляем маршрут к виртуалке А через виртуалку В
+echo '# создаем адаптер с типом bridge'
+ip link add macvlan1 link eth0 type macvlan mode bridge 
+echo '# ip адрес адаптера'
+ip address add dev macvlan1 $IPADDR 
+echo '# включаем адаптер'
+ip link set macvlan1 up 
+
+echho '# добавляем маршрут к виртуалке А через виртуалку В'
+ip route add $SUBNET viA $GETEWAY 
 
 curl 'http://192.168.28.10:5000/'
