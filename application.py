d74adeeb0aaaed158da73528cc67a93a981bd4e2
Po <polina.zhirakova@gmail.com>
2024-03-07 18:21:55 +0300

    Исправлена кодировка файла application.py.

--- application.py
+++ application.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_():
-    return "Был получен GET запрос\n\n"
+    return "GET-request has been gotten\n\n"
 
 @app.route("/<number>", methods =['POST'])
 def post_(number):
-    return f"POST запрос : {number}\n\n"
+    return f"POST-request : {number}\n\n"
 
 @app.route("/", methods =['PUT'])
 def put():
-    return "Был получен PUT запрос\n\n"
+    return "PUT-request has been gotten\n\n"
 
 app.run(host='0.0.0.0', port=5000)
